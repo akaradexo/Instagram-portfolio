8c8a849fb6c81554b44ff128740b54c8f08c3fb3
xR0v3Rx <infoashu.k@gmail.com>
2021-06-24 18:56:35 +0530

    skda

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Story from './components/Story';
 import Highlight from './components/Highlight';
+import Social from './components/Social';
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" component={Header}/>
         <Route  path="/story" component={Story}/>
         <Route  path="/highlight" component={Highlight}/>
+        <Route  path="/social" component={Social}/>
       </Switch>
      
         
---
+++ src/components/Social.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <social>
+            <div className="social">
+               
+            </div>
+        </social>
+    )
+}
+
+export default Social
