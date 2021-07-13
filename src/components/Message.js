d371cb49fa21cee0005c2c289d9679e2b23c8c45
xR0v3Rx <69238714+xR0v3Rx@users.noreply.github.com>
2021-07-13 11:00:54 +0530

    Add files via upload

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import React from 'react';
+import emailjs from 'emailjs-com';
 const Message = () => {
+    function sendEmail(e){
+        e.preventDefault();
+
+        emailjs.sendForm('service_ne9afzb','template_uq87bia', e.target, 'user_QQtFoMIyo5flop7JoTFNE')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+    }
     return (
-        <message>
+        <>
 
             <div className="follow">
                 <h1>NEVER MISS A THING</h1>
@@ -14,15 +25,15 @@ const Message = () => {
                         <p><a href="infoashu.k@gmail.com">infoashu.k@gmail.com</a></p>
                         <p>@ 2023 by Ashyou. make sure you follow me on Instagram</p>
                     </div>
-                    <div className="form">
-                        <input type="text" placeholder="Name"></input>
-                        <input type="Email" placeholder="Email"></input>
-                        <input type="text" style={{height:"10rem"}} placeholder="Type your message here..."></input>
-                        <button type="submit">Submit</button>
-                    </div>
+                    <form onSubmit={sendEmail}>
+                        <input type="text" placeholder="Name" name="from_name"></input>
+                        <input type="Email" placeholder="Email" name="email"></input>
+                        <input type="text" style={{height:"10rem"}} placeholder="Type your message here..." name="message"></input>
+                        <button type="submit" >Submit</button>
+                    </form>
                 </div>
-            </div>
-        </message>
+            </div>  
+        </>
     )
 }
 
